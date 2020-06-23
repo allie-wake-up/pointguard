9e8be7390da0f0ff906fce8f4a253aba87894e7a
Andrew Stephan <thekiteeatingtree@gmail.com>
2020-06-22 22:57:36 -0400

    Rename to point guard
    - add config and clap as dependencies
    - play around with clap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,73 @@
+use clap::Clap;
+
+/// This doc string acts as a help message when the user runs '--help'
+/// as do all doc strings on fields
+#[derive(Clap)]
+#[clap(version = "0.1.0", author = "Andrew Stephan<andrew@pointguard.dev>")]
+struct Opts {
+    /// Sets a custom config file. Could have been an Option<T> with no default too
+    #[clap(short, long, default_value = "default.conf")]
+    config: String,
+    /// Some input. Because this isn't an Option<T> it's required to be used
+    input: Option<String>,
+    /// A level of verbosity, and can be used multiple times
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: i32,
+    #[clap(subcommand)]
+    subcmd: Option<SubCommand>,
+}
+
+#[derive(Clap)]
+enum SubCommand {
+    #[clap(name = "test")]
+    Test(Test),
+    #[clap(name="show", aliases = &["ls", "show"])]
+    List(List),
+}
+
+/// A subcommand for controlling testing
+#[derive(Clap)]
+struct Test {
+    /// Print debug info
+    #[clap(short)]
+    debug: bool,
+}
+
+/// A subcommand for listing password files
+#[derive(Clap)]
+struct List {}
+
 fn main() {
-    println!("Hello, world!");
+    let opts: Opts = Opts::parse();
+
+    // Gets a value for config if supplied by user, or defaults to "default.conf"
+    println!("Value for config: {}", opts.config);
+    println!("Using input file: {}", opts.input.unwrap_or_default());
+
+    // Vary the output based on how many times the user used the "verbose" flag
+    // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
+    match opts.verbose {
+        0 => println!("No verbose info"),
+        1 => println!("Some verbose info"),
+        2 => println!("Tons of verbose info"),
+        3 | _ => println!("Don't be crazy"),
+    }
+
+    // You can handle information about subcommands by requesting their matches by name
+    // (as below), requesting just the name used, or both at the same time
+    match opts.subcmd {
+        Some(cmd) => match cmd {
+            SubCommand::Test(t) => {
+                if t.debug {
+                    println!("Printing debug info...");
+                } else {
+                    println!("Printing normally...");
+                }
+            }
+            SubCommand::List(t) => {
+                println!("Print out the password files");
+            }
+        },
+        None => println!("Print out the password files")
+    }
 }
