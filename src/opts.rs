27b84e3390251ca76c4533f51637d70eefba75ee
Allie Stephan <thekiteeatingtree@gmail.com>
2020-08-02 20:37:19 -0400

    Flatten show subcommand options into main options
    - split clip code in show out into its own function

--- src/opts.rs
+++ src/opts.rs
@@ -3,31 +3,35 @@ use clap::Clap;
 /// This doc string acts as a help message when the user runs '--help'
 /// as do all doc strings on fields
 #[derive(Clap)]
-#[clap(version = "0.1.0", author = "Andrew Stephan<andrew@pointguard.dev>")]
+#[clap(version = "0.1.0", author = "Allie Stephan<allie@pointguard.dev>")]
 pub struct Opts {
     /// Sets a custom config file. Could have been an Option<T> with no default too
     // #[clap(short, long, default_value = "default.conf")]
     // config: String,
-    /// Some input
-    pub input: Option<String>,
     /// A level of verbosity, and can be used multiple times
     // #[clap(short, long, parse(from_occurrences))]
     // verbose: i32,
     #[clap(subcommand)]
     pub subcmd: Option<SubCommand>,
+    #[clap(flatten)]
+    pub show: Show,
 }
 
 #[derive(Clap)]
 pub enum SubCommand {
     #[clap(name = "clip")]
-    Clip,
+    Clip(Clip),
     #[clap(name="show", aliases = &["ls", "show"])]
     Show(Show),
 }
 
 /// A subcommand for copying to the clipboard
 #[derive(Clap)]
-pub struct Clip {}
+pub struct Clip {
+    /// The number of seconds to copy the input
+    /// for before clearing the clipboard
+    pub clip_time: u64,
+}
 
 /// A subcommand for listing password files
 #[derive(Clap)]
